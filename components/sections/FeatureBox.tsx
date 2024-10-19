dd61a1e0aed16713079380a9d9de77d9b2abcc60
adel mekki <webtechwizzard@gmail.com>
2024-10-19 23:33:52 +0100

    fix useEffect rerender bug

--- components/sections/FeatureBox.tsx
+++ components/sections/FeatureBox.tsx
@@ -1,12 +1,10 @@
 import {CSSProperties, useEffect, useRef, useState} from "react";
-import G2 from "../gradients/G2";
 
 type props = {
   feature: any;
   mouse: {x: number; y: number};
-  main?: boolean;
 };
-export default function FeatureBox({feature, mouse, main}: props) {
+export default function FeatureBox({feature, mouse}: props) {
   const element = useRef<HTMLDivElement>(null);
   const [position, setPosition] = useState({top: 0, left: 0});
 
@@ -21,16 +19,11 @@ export default function FeatureBox({feature, mouse, main}: props) {
   };
 
   useEffect(() => {
-    updatePosition();
-
-    window.addEventListener("scroll", updatePosition);
-    window.addEventListener("resize", updatePosition);
-
+    window.addEventListener("pointermove", updatePosition);
     return () => {
-      window.removeEventListener("scroll", updatePosition);
-      window.removeEventListener("resize", updatePosition);
+      window.removeEventListener("pointermove", updatePosition);
     };
-  }, [mouse]);
+  }, []);
   return (
     <div
       className={`rounded-3xl p-0.5 bg-background relative border-glow overflow-hidden feature-box`}
